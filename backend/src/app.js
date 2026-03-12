e3cbbb2bbd6c560a29150b1186061a6373f0d11f
prachi savner <prachisavner24@gmail.com>
2026-03-12 15:13:58 +0530

    complete

---
+++ backend/src/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+import connectToSocket from "./controllers/socketManager.js";
+
+const app = express();
+
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.port || 8080);
+
+
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb",extended:true}));
+app.use("/api/v1/users",userRoutes);
+
+
+// app.set("mongo_username");
+
+const start = async () => {
+  const connectionDb = await mongoose.connect(
+    "mongodb+srv://zoom_clone:zoom_clone@zoomclonecluster.wojmx7o.mongodb.net/",
+  );
+  
+  console.log(`mongo connected db host  : ${connectionDb.connection.host}`);
+  server.listen(app.get("port"), () => {
+    console.log("listening to the port 8080");
+  });
+};
+
+start();
